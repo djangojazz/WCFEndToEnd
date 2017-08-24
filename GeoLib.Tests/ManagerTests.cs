cce67b2755e780f46f7d40828d6f35ea38ffc587
Brett Morin <brettmorin2@gmail.com>
2017-08-24 14:39:21 -0700

    Made changes to complete Module 3

---
+++ GeoLib.Tests/ManagerTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Moq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GeoLib.Data;
+using GeoLib.Contracts;
+using GeoLib.Services;
+
+namespace GeoLib.Tests
+{
+  [TestClass]
+  public class ManagerTests
+  {
+    [TestMethod]
+    public void test_zip_code_retrieval()
+    {
+      Mock<IZipCodeRepository> mockZipCodeRepository = new Mock<IZipCodeRepository>();
+
+      ZipCode zipCode = new ZipCode()
+      {
+        City = "Portland",
+        State = new State() { Abbreviation = "OR"},
+        Zip = "97035"
+      };
+
+      mockZipCodeRepository.Setup(obj => obj.GetByZip("97035")).Returns(zipCode);
+
+      IGeoService geoService = new GeoManager(mockZipCodeRepository.Object);
+
+      ZipCodeData data = geoService.GetZipInfo("97035");
+
+      Assert.IsTrue(data.City.ToUpper() == "PORTLAND");
+      Assert.IsTrue(data.State == "OR");
+    }
+  }
+}
