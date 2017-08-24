b54b62c26b28808781cb0d5f00dd57034def3d0d
Brett Morin <brettmorin2@gmail.com>
2017-08-24 16:35:09 -0700

    Finished up Module 4

--- Hosting/GeoLib.ConsoleHost/Program.cs
+++ Hosting/GeoLib.ConsoleHost/Program.cs
@@ -1,8 +1,10 @@
-﻿using GeoLib.Services;
+﻿using GeoLib.Contracts;
+using GeoLib.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel;
+using System.ServiceModel.Channels;
 
 namespace GeoLib.ConsoleHost
 {
@@ -11,6 +13,13 @@ namespace GeoLib.ConsoleHost
     static void Main(string[] args)
     {
       ServiceHost hostGeoManager = new ServiceHost(typeof(GeoManager));
+
+      //Added without config file
+      string address = "net.tcp://localhost:8009/GeoService";
+      Binding binding = new NetTcpBinding();
+      Type contract = typeof(IGeoService);
+      hostGeoManager.AddServiceEndpoint(contract, binding, address);
+
       hostGeoManager.Open();
 
       Console.WriteLine("Services started. Press [Enter] to exit.");
