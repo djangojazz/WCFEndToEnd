d6ff237d3045e2925ea4e282941c9a2638090b43
Brett Morin <brettmorin2@gmail.com>
2017-08-24 11:34:41 -0700

    First Stage

--- GeoLib.Core/DataExtensions.cs
+++ GeoLib.Core/DataExtensions.cs
@@ -9,6 +9,7 @@ namespace GeoLib.Core
         public static IEnumerable<T> ToFullyLoaded<T>(this IQueryable<T> query)
         {
             return query.ToArray().ToList();
+
         }
 
         public static IEnumerable<T> ToFullyLoaded<T>(this IEnumerable<T> enumerable)
