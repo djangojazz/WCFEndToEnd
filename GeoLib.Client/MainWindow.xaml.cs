bbbbf242b006feaf002956d4cb149b80c16c5267
Brett Morin <brettmorin2@gmail.com>
2017-08-24 17:03:32 -0700

    Got client working in Module 5

--- GeoLib.Client/MainWindow.xaml.cs
+++ GeoLib.Client/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GeoLib.Contracts;
+using GeoLib.Proxies;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -7,35 +9,43 @@ using System.Windows;
 
 namespace GeoLib.Client
 {
-    public partial class MainWindow : Window
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
+      InitializeComponent();
 
-            this.Title = "UI Running on Thread " + Thread.CurrentThread.ManagedThreadId +
-                " | Process " + Process.GetCurrentProcess().Id.ToString();
-        }
-
-        private void btnGetInfo_Click(object sender, RoutedEventArgs e)
-        {
-            if (txtZipCode.Text != "")
-            {
-
-            }
-        }
-
-        private void btnGetZipCodes_Click(object sender, RoutedEventArgs e)
-        {
-            if (txtState.Text != null)
-            {
-
-            }
-        }
-
-        private void btnMakeCall_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
+      this.Title = "UI Running on Thread " + Thread.CurrentThread.ManagedThreadId +
+          " | Process " + Process.GetCurrentProcess().Id.ToString();
     }
+
+    private void btnGetInfo_Click(object sender, RoutedEventArgs e)
+    {
+      if (txtZipCode.Text != "")
+      {
+        GeoClient proxy = new GeoClient();
+        ZipCodeData data = proxy.GetZipInfo(txtZipCode.Text);
+        if(data != null)
+        {
+          lblCity.Content = data.City;
+          lblState.Content = data.State;
+        }
+
+        proxy.Close();
+      }
+    }
+
+    private void btnGetZipCodes_Click(object sender, RoutedEventArgs e)
+    {
+      if (txtState.Text != null)
+      {
+
+      }
+    }
+
+    private void btnMakeCall_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+  }
 }
